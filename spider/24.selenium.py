ac9efdbf419376e93347622c6adcd845919d536e
LMH-ironman <970259067@qq.com>
2023-07-25 19:30:33 +0800

    se

---
+++ spider/24.selenium.py
@@ -0,0 +1,11 @@
+# import urllib.request
+# from selenium import webdriver
+#
+#
+# path = 'chromedriver.exe'
+#
+# browser = webdriver.Chrome(path)
+#
+# url = 'https://www.baidu.com/'
+#
+# browser.get(url)
